5ef8b7820446ac54b4b8f4c75a1f3d6ea91d4655
Rahul <rahuldavu23@gmail.com>
2025-05-27 22:33:11 -0400

    I think I fixed the errors

--- src/objects.ts
+++ src/objects.ts
@@ -30,12 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    answer = answer.trim().toLowerCase();
-    if (answer === question.expected) {
-        return true;
-    } else {
-        return false;
-    }
+    return (
+        answer.trim().toLowerCase() === question.expected.trim().toLowerCase()
+    );
 }
 
 /**
